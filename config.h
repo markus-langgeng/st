6e95c54f3adb7c39f14311dd966f3a13e8871012
markus-langgeng <markus.langgeng4@gmail.com>
2023-12-03 12:54:28 +0700

    idk made some changes

--- config.h
+++ config.h
@@ -8,6 +8,7 @@
 static char *font = "Liberation Mono:pixelsize=13:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
+  "Liberation Mono:pixelsize=13:antialias=true:autohint=true",
   "Symbols Nerd Font:pixelsize=11:antialias=true:autohint=true",
   "Noto Color Emoji:pixelsize=11:antialias=true:autohint=true",
   "Noto Sans CJK JP:pixelsize=13:antialias=true:autohint=true",
